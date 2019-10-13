35f182d517a1bfcc1307b0a6b7071f8867209b19
kknosala <51036712+kknosala@users.noreply.github.com>
2019-10-13 09:56:45 -0600

    initial htmlRoutes created and working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,5 +3,17 @@ var db = require("../models");
 module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index');
+    });
+
+    app.get('/portfolio', function(req, res) {
+        res.render('portfolio');
+    });
+
+    app.get('/contact', function(req, res) {
+        res.render('contact');
+    });
+
+    app.get('/recomendations', function(req, res) {
+        res.render('recomend');
     })
 }
